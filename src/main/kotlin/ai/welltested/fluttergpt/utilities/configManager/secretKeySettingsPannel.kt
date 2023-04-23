8872f467f7ca1a27f44de5b5be0887adba855bdc
Keval Prajapati <keval@Kevals-Mac-mini.local>
2023-04-23 14:31:30 +0530

    ui: updates settings page UI

--- src/main/kotlin/ai/welltested/fluttergpt/utilities/configManager/secretKeySettingsPannel.kt
+++ src/main/kotlin/ai/welltested/fluttergpt/utilities/configManager/secretKeySettingsPannel.kt
@@ -1,28 +1,55 @@
 package ai.welltested.fluttergpt.utilities.configManager
 
-import java.awt.GridBagConstraints
-import java.awt.GridBagLayout
-import java.awt.Insets
+import java.awt.Dimension
+import javax.swing.Box
+import javax.swing.BoxLayout
 import javax.swing.JLabel
 import javax.swing.JPanel
 import javax.swing.JTextField
+import java.awt.FlowLayout
+import java.awt.Font
+import java.awt.Insets
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
+import java.awt.Desktop
+import java.net.URI
 
 class SecretKeySettingsPanel : JPanel() {
     private val secretKeyTextField: JTextField
 
     init {
-        layout = GridBagLayout()
+        layout = BoxLayout(this, BoxLayout.Y_AXIS)
 
-        val label = JLabel("Secret Key:")
-        val constraints = GridBagConstraints().apply {
-            anchor = GridBagConstraints.WEST
-            insets = Insets(4, 4, 4, 4)
+        val titleLabel = JLabel("Fluttergpt: Api Key").apply {
+            font = Font(font.name, Font.BOLD, font.size + 2)
+            alignmentX = LEFT_ALIGNMENT
         }
-        add(label, constraints)
+        add(titleLabel)
 
-        secretKeyTextField = JTextField(25)
-        constraints.gridx = 1
-        add(secretKeyTextField, constraints)
+
+
+        val openAiLabel = JLabel("<html>Key from <a href=\"\">OpenAI</a></html>").apply {
+            cursor = java.awt.Cursor.getPredefinedCursor(java.awt.Cursor.HAND_CURSOR)
+            addMouseListener(object : MouseAdapter() {
+                override fun mouseClicked(e: MouseEvent) {
+                    Desktop.getDesktop().browse(URI.create("https://platform.openai.com/account/api-keys"))
+                }
+            })
+            alignmentX = LEFT_ALIGNMENT
+            maximumSize = Dimension(200, preferredSize.height)
+        }
+        add(openAiLabel)
+
+        add(Box.createRigidArea(Dimension(0, 10))) // Add vertical space
+
+        secretKeyTextField = JTextField(25).apply {
+            alignmentX = LEFT_ALIGNMENT
+            margin = Insets(2, 0, 2, 0)
+            maximumSize = Dimension(400, preferredSize.height)
+        }
+        add(secretKeyTextField)
+
+        add(Box.createVerticalGlue())
     }
 
     var secretKey: String
